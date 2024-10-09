de7bf5409af2790394d049da34d94392c03aeea9
Endeavour <martin.wonders@unn.ac.uk>
2024-10-09 20:13:38 +0100

    Opened a window using variables and user input

--- KV5043_Lab2_Exercises/main.cpp
+++ KV5043_Lab2_Exercises/main.cpp
@@ -4,11 +4,12 @@
 
 int main(int argc, char* argv[])
 {
-	const std::string windowName = "Test Window";
-	int x = 100;
-	int y = 300;
-	int width = 800;
-	int height = 600;
+	std::string windowName;
+	int x, y, width, height;
+	std::cout << "Enter the window name: ";
+	std::getline(std::cin, windowName);
+	std::cout << "Enter the window paremeters (x y width height): ";
+	std::cin >> x >> y >> width >> height;
 	Window myWindow = Window(windowName, x, y, width, height);
 	myWindow.Run();
 	return 0;
