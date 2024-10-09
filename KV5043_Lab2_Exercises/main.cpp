9a47030c84eedd5b45ae8f63aab4105e05a6a0c4
Endeavour <martin.wonders@unn.ac.uk>
2024-10-09 20:06:39 +0100

    Opened a window using variables

--- KV5043_Lab2_Exercises/main.cpp
+++ KV5043_Lab2_Exercises/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <string>
 #include "simple_window.h"
 
 int main(int argc, char* argv[])
 {
-	Window myWindow = Window("test", 100, 300, 800, 600);
+	const std::string windowName = "Test Window";
+	int x = 100;
+	int y = 300;
+	int width = 800;
+	int height = 600;
+	Window myWindow = Window(windowName, x, y, width, height);
 	myWindow.Run();
 	return 0;
 }
